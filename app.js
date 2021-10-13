6e70c2716b1ca0b5f23947698084aab161d9d722
sdesire <sdesire@ltu.edu>
2021-10-13 13:19:22 -0400

    Learning basics of node

---
+++ app.js
@@ -0,0 +1,14 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+    for(let i = 0; i < profileDataArr.length; i++) {
+        console.log(ProfileDataArr[i]);   
+    }
+
+    console.log('==============');
+
+    // Is the same as this...
+    profileDataArr.forEach(profileItem =>console.log(profileItem));
+};
+
